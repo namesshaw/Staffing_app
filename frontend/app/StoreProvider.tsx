034188cc98d96764d637c1e9c022bddd0ef5ea6d
cosmic-1234 <shrirangswanikar@gmail.com>
2025-05-01 12:03:47 +0530

    Everthing is in sync and working Great

--- frontend/app/StoreProvider.tsx
+++ frontend/app/StoreProvider.tsx
@@ -1,18 +1,25 @@
 'use client'
+
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 import { makeStore, AppStore } from '../public/store'
+import { Persistor } from 'redux-persist/es/types'
 
 export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const storeRef = useRef<AppStore>(undefined)
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore()
-  }
+  const storeRef = useRef<AppStore>(makeStore())
+  const persistorRef = useRef<Persistor>(persistStore(storeRef.current))
 
-  return <Provider store={storeRef.current}>{children}</Provider>
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={<div>Loading...</div>} persistor={persistorRef.current}>
+        {children}
+      </PersistGate>
+    </Provider>
+  )
 }

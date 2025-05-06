611349afdc61792b9a3d9e2e43d4bad21c3ad9b1
cosmic-1234 <shrirangswanikar@gmail.com>
2025-05-06 17:06:20 +0530

    Good game

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const db_1 = __importDefault(require("../src/db/db"));
+const db_1 = __importDefault(require("../dist/db/db"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         // Example skill sets
